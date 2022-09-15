18195f5cc8751416a8b40c0f3a6e799a563c9abc
Diogo Lima <diogoslva0133@gmail.com>
2022-09-15 12:46:48 -0300

    discovering the functionality of queryselectAll

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -21,8 +21,14 @@ class CalcController {
 
         }, 1000); 
 
-    
+        
     }
+
+        initButtonsEvents(){
+            let buttons = document.querySelectorAll("#buttons > g, #parts > g")
+            
+        }
+
         //METÓDOS
     setDisplayDateTime(){
         this.displayDate = this.currentDate.toLocaleDateString(this._locale);
