40db89943d8de828750a0829ab515de287d786e3
Diogo Lima <diogoslva0133@gmail.com>
2022-09-16 08:32:45 -0300

    applying multiple events

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -25,13 +25,31 @@ class CalcController {
         
     }
 
+    addEventListenerAll(element, events, fn) { 
+
+        events.split('  ').forEach(event => {
+
+            element.addEventListener(event, fn , false );
+        
+        });
+       
+    }
+
         initButtonsEvents(){
+            
             let buttons = document.querySelectorAll("#buttons > g, #parts > g")
 
-          buttons.forEach(btn=>{
-            btn.addEventListener('click', e => {
+          buttons.forEach((btn, index)=>{
+            this.addEventListenerAll(btn, "click  drag", e => {
                 console.log(btn.className.baseVal.replace("btn-","" ));
             });
+
+            this.addEventListenerAll(btn, "mouseover  mouseup  mousedown", e =>{
+
+                btn.style.cursor = "pointer"
+            })
+
+
           })
 
         }
